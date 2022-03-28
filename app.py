a82821ca4c37998d898b53b5f515eb99da62eecb
Fin <tdtatum2@gmail.com>
2022-03-28 09:11:48 -0500

    Forming Admin Pages. (Nonfunctional)

--- app.py
+++ app.py
@@ -12,4 +12,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+
+@app.route("/event_manager")
+def event_manager():
+    return render_template('event_manager.html')
+
+@app.route("/gallery_manager")
+def gallery_manager():
+    return render_template('gallery_manager.html')
+
+@app.route("/officer_manager")
+def officer_manager():
+    return render_template('officer_manager.html')
+
 app.run(debug=True)
