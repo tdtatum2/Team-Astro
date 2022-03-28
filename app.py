1b2253a8ca1e526ceef10c50d649d1dd4bb33de3
Fin <tdtatum2@gmail.com>
2022-03-28 11:13:16 -0500

    Forming Admin Pages. (Nonfunctional)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import sqlite3
 import os
-
+from wtforms import TextAreaField, StringField, validators, PasswordField, Form, SubmitField, HiddenField, SelectField
 
 app = Flask(__name__)
 
@@ -16,17 +16,41 @@ def index():
 def admin():
     return render_template('admin.html')
 
+class AddEvent(Form):
+    event = StringField('Event Name', [validators.Length(min=1, max=30)])
+    location = StringField('Event Location', [validators.Length(min=1, max=30)])
+    time = StringField('Event Time', [validators.Length(min=1, max=30)])
+    description = StringField('Event Description', [validators.Length(min=1, max=75)])
 
-@app.route("/event_manager")
+
+@app.route("/event_manager", methods=['GET', 'POST'])
 def event_manager():
-    return render_template('event_manager.html')
+    form = AddEvent(request.form)
+    return render_template('event_manager.html', form=form)
+
+
+class AddImage(Form):
+    image = StringField('Image File', [validators.Length(min=1, max=30)])
+    title = StringField('Image Title', [validators.Length(min=1, max=30)])
+    description = StringField('Image Description', [validators.Length(min=1, max=75)])
 
 @app.route("/gallery_manager")
 def gallery_manager():
-    return render_template('gallery_manager.html')
+    form = AddImage(request.form)
+    return render_template('gallery_manager.html', form=form)
+
+
+class AddOfficer(Form):
+    username = StringField('Username', [validators.Length(min=1, max=30)])
+    password = PasswordField('Password', [
+            validators.DataRequired(),
+            validators.EqualTo('confirm', message='Passwords do not match.')
+        ])
+    confirm = PasswordField('Confirm Password')
 
 @app.route("/officer_manager")
 def officer_manager():
-    return render_template('officer_manager.html')
+    form = AddOfficer(request.form)
+    return render_template('officer_manager.html', form=form)
 
 app.run(debug=True)
