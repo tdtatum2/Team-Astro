449022736987806d49a2df5bd5774713cf1e1b05
Fin <tdtatum2@gmail.com>
2022-03-29 20:44:57 -0500

    Form creations. Working on functionality in admin pages.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, session, request, flash
+from tokenize import String
+from flask import Flask, render_template, session, request, flash, redirect, url_for
 import sqlite3
 import os
+from os.path import join, dirname, abspath
 from wtforms import TextAreaField, StringField, validators, PasswordField, Form, SubmitField, HiddenField, SelectField
+import bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Sg4&gLK**23S'
+UPLOADS_PATH = join(dirname(abspath(__file__)), 'static/uploads/')
+UPLOAD_FOLDER = 'app/static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_PATH'] = 157286400
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 @app.route("/")
@@ -34,7 +46,7 @@ class AddImage(Form):
     title = StringField('Image Title', [validators.Length(min=1, max=30)])
     description = StringField('Image Description', [validators.Length(min=1, max=75)])
 
-@app.route("/gallery_manager")
+@app.route("/gallery_manager", methods=['GET', 'POST'])
 def gallery_manager():
     form = AddImage(request.form)
     return render_template('gallery_manager.html', form=form)
@@ -48,9 +60,81 @@ class AddOfficer(Form):
         ])
     confirm = PasswordField('Confirm Password')
 
-@app.route("/officer_manager")
+class EditOfficer(Form):
+    id = HiddenField()
+    username = StringField('Username', [validators.Length(min=1, max=30)])
+    password = PasswordField('Password', [
+            validators.DataRequired(),
+            validators.EqualTo('confirm', message='Passwords do not match.')
+        ])
+    confirm = PasswordField('Confirm Password')
+    update_user = SubmitField()
+    remove_user = SubmitField()
+
+@app.route("/officer_manager", methods=['GET', 'POST'])
 def officer_manager():
-    form = AddOfficer(request.form)
-    return render_template('officer_manager.html', form=form)
+    add = AddOfficer(request.form)
+    conn = get_db_connection()
+    officers = conn.execute("SELECT * FROM officers").fetchall()
+    conn.close()
+    if request.method == 'POST' and add.data:
+        flash('The add button was clicked', 'success')
+        return render_template('officer_manager.html', officers=officers, add=add)
+    return render_template('officer_manager.html', officers=officers, add=add)
+
+class Login(Form):
+    username = StringField('Username', [validators.Length(min=1, max=30)])
+    password = PasswordField('Password', [validators.DataRequired()])
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = Login(request.form)
+    try:
+        if session['loggedIn'] == True:
+            flash('You are already logged in!', 'warning')
+            return redirect(url_for('index'))
+        else:
+            if request.method == 'POST' and form.validate():
+                try:
+                    username = form.username.data
+                    password = form.password.data
+                    conn = get_db_connection()
+                    user = conn.execute('SELECT * FROM users WHERE username = ?',
+                                            (username,)).fetchone()
+                    dbPassword = user['password']
+                    conn.close()
+                    if bcrypt.checkpw(password.encode('utf-8'), dbPassword.encode('utf-8')):
+                        session['loggedIn'] = True
+                        flash('Login successful', 'success')
+                        return redirect(url_for('admin'))
+                    else:
+                        flash('Username or password is incorrect!', 'danger')
+                        return render_template('login.html', form=form)
+                except:
+                    flash('Username or password is incorrect!', 'danger')
+                    return render_template('login.html', form=form)
+            return render_template('login.html', form=form)
+    except:
+        if request.method == 'POST' and form.validate():
+                try:
+                    username = form.username.data
+                    password = form.password.data
+                    conn = get_db_connection()
+                    user = conn.execute('SELECT * FROM users WHERE username = ?',
+                                            (username,)).fetchone()
+                    dbPassword = user['password']
+                    conn.close()
+                    if bcrypt.checkpw(password.encode('utf-8'), dbPassword):
+                        session['loggedIn'] = True
+                        flash('Login successful', 'success')
+                        return redirect(url_for('admin'))
+                    else:
+                        flash('Username or password is incorrect!', 'danger')
+                        return render_template('login.html', form=form)
+                except:
+                    flash('Username or password is incorrect!', 'danger')
+                    return render_template('login.html', form=form)
+        return render_template('login.html', form=form)
+
 
 app.run(debug=True)
